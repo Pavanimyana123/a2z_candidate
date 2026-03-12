258dfb0294b5506e556c74a4ef175419cffa3628
pavanimyana123 <pavanimyana2000@gmail.com>
2026-03-12 09:47:26 +0530

    Pavani

--- src/components/Admin/Layout/Header.js
+++ src/components/Admin/Layout/Header.js
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { FaBars, FaBell, FaSearch } from "react-icons/fa";
 
 const Header = () => {
+  const userData = JSON.parse(localStorage.getItem("admin_user"));
+  const userName = userData ? `${userData.full_name}` : "Admin";
+
+  useEffect(() => {
+    document.title = userData?.full_name ? `${userData.full_name} (Admin)` : "A2Z ";
+  }, [userData]);
+
   return (
     <header className="ta-header">
       <div className="d-flex align-items-center gap-3">
